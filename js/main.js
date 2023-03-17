0b33c40465fb02330227e8656ccdeb6b721337b7
RUDYGAMES <rudygamesv2@gmail.com>
2023-03-17 23:24:35 +0100

    change nav: added new functions in mobile

--- js/main.js
+++ js/main.js
@@ -1,24 +1,27 @@
-const navMobile =  document.querySelector('.nav-mobile');
-const navBtn =  document.querySelector('.hamburger');
+const navMobile = document.querySelector('.nav-mobile')
+const navBtn = document.querySelector('.hamburger')
 const footerYear = document.querySelector('.footer__year')
-
+const allNavLinks = document.querySelectorAll('.nav__link')
 
 const handleNav = () => {
-    navBtn.classList.toggle('is-active');
-    navMobile.classList.toggle('nav-mobile--active');
-    document.body.classList.toggle('sticky-body');
+	navBtn.classList.toggle('is-active')
+	navMobile.classList.toggle('nav-mobile--active')
+	document.body.classList.toggle('sticky-body')
+
+	allNavLinks.forEach(item => {
+		item.addEventListener('click', () => {
+			navBtn.classList.remove('is-active')
+			navMobile.classList.remove('nav-mobile--active')
+			document.body.classList.remove('sticky-body')
+		})
+	})
 }
-
-
-
-
-
 
 navBtn.addEventListener('click', handleNav)
 
 const handleCurrentYear = () => {
-    const year = (new Date).getFullYear();
-    footerYear.innerText = year;
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
 }
 
 handleCurrentYear()
